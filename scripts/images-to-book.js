64a3944f01300315c21a4f39a020d5429c06998a
Илья С <skachilya@gmail.com>
2025-08-17 17:51:37 +0300

    book: pdf→images (232 pages)

---
+++ scripts/images-to-book.js
@@ -0,0 +1,12 @@
+import fs from 'fs/promises';
+
+const dir = 'webapp/assets/book-pages';
+const files = (await fs.readdir(dir))
+  .filter(n => /\.(jpe?g|png|webp)$/i.test(n))
+  .sort();
+const pages = files.map((n, i) => ({
+  type: i < 2 ? 'demo' : 'full',
+  content: `<img class="full-img" src="/assets/book-pages/${n}"/>`
+}));
+await fs.writeFile('webapp/assets/book.json', JSON.stringify({ pages }, null, 2));
+console.log('OK pages:', files.length);
